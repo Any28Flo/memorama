d59259cd266693706c752a1a46e30283d5ffbc12
Analin Flores <foglzerika@gmail.com>
2017-11-04 11:08:44 -0600

    Creando funciones iniciales

---
+++ README.md
@@ -0,0 +1,9 @@
+## SimonDice
+
+Es una aplicación simulando el clásico **Simón dice** es un juego que te indicará qué teclas debes tocar, debes memorizarlas y hacer todo lo que Simón dice.
+
+## Créditos
+-[Analin Flores] (https://twitter.com/@any28flo)
+
+## Licencia
+[MIT] (https://opensource.org/licenses/MIT)
--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-const tecla=document.getElementsByClassName('key');
-document.addEventListener("keydown",function(e){console.log(e)});
+function cargaPagina(){
+  const niveles = 15
+  let teclas = generarTeclas(niveles)
+
+  function generarTeclas(niveles) {
+    return new Array(niveles).fill(0).map(generarTeclaAleatoria)
+  }
+
+  function generarTeclaAleatoria() {
+    const min = 65
+    const max = 90
+    return Math.round(Math.random() * (max - min) + min)
+  }
+
+  function getElementByKeyCode(keyCode) {
+    return document.querySelector(`[data-key="${keyCode}"]`)
+  }
+
+  function activate(keyCode, opts = {}) {
+    const el = getElementByKeyCode(keyCode)
+    el.classList.add('active')
+    if (opts.success) {
+      el.classList.add('success')
+    } else if (opts.fail) {
+      el.classList.add('fail')
+    }
+    setTimeout(() => deactivate(el), 500)
+  }
+
+  function deactivate(el) {
+    el.className = 'key'
+  }
+  document.addEventListener("keydown", function(e){
+    console.log(e.keyCode);
+     activate(e.keyCode);
+  })
+}
+cargaPagina();
